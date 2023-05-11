048c8a0c2b70b36ac6b8478d2ecfec9f1511cd2d
Matheus <matheusbatistaum@gmail.com>
2023-05-11 15:24:43 -0300

    Update finances.cy.js

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,2 +1,16 @@
 
+it('Excluir transação', () => {
+    cy.visit('https://devfinance-agilizei.netlify.app/#')
+    cy.contains("Nova Transação").click()
+    cy.get('#description').type("Freela")
+    cy.get('#amount').type(100)
+    cy.get('#date').type("2023-05-05")
+    cy.contains('button', 'Salvar').click()
+
+    cy.contains(".description", "Freela")
+    .parent()
+    .find("img")
+    .click()
+    
+});
 
